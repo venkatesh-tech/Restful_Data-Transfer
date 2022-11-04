c488a79653ae1a3576e396c407baf7d798d77e14
Venkateshe <kolapallinagavenkatesh@gmail.com>
2022-11-04 12:07:05 +0530

    Initial Commit

---
+++ src/main/java/com/rest/fileprocessing/RestfileprocessingApplication.java
@@ -0,0 +1,13 @@
+package com.rest.fileprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfileprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfileprocessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rest/fileprocessing/RestfileprocessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.fileprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfileprocessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
